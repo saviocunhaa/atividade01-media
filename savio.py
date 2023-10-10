ab353be7580831dfc0507ae7c8206b86ec4f410a
saviocunha <saviocunha.001@hotmail.com>
2023-10-10 16:51:14 -0300

    adicionado recurso de input

--- savio.py
+++ savio.py
@@ -1,8 +1,8 @@
 # media de 3 numeros
 # receber as 3 notas
-nota1 = 8.0
-nota2 = 7.5
-nota3 = 4.7
+nota1 = float(input("digite a nota 01"))
+nota2 = float(input(6"digite a nota 02"))
+nota3 = float(input("digite a nota 03"))
 # Somar as notas e dividir por 3
 media = (nota1 + nota2 + nota3) / 3
 # apresentar a media
